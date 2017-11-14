a870a44dfe781340310b7958f2578830ce4195e7
zhejiangzhoulei <zhejiangzhoulei@163.com>
2017-11-14 08:13:55 +0800

    update: run.bat

--- UKMO_UAVRouting/Main.cpp
+++ UKMO_UAVRouting/Main.cpp
@@ -5,8 +5,13 @@
 
 #include"BFS.h"
 
-int main()
+//int main()
+//{
+int main(int argc, char * argv[])
 {
+	cout << "Argv[0]: " << argv[0] << "\t";
+	cout << "Argv[1]: " << argv[1] << "\t";
+	cout << "Argv[2]: " << argv[2] << endl;
 	////sjy
 	//Block * testBlock = new Block(100,100,100);
 	//BFS * testBFS = new BFS(testBlock);
@@ -16,13 +21,13 @@ int main()
 	//zhoulei
 	Schedule* sche = new Schedule();
 	IO* io = new IO();
-	int date = 1;
+	int date = atoi(argv[1]);
 	io->input(sche, date);
 	
 	sche->init();
 
 	Block* origin = sche->getCity(0);//the blcok corresponding to origin
-	int cityNo = 1;
+	int cityNo = atoi(argv[2]);
 	Block* destination = sche->getCity(cityNo);//the blcok corresponding to the destination
 	vector<Block*> blocks = sche->getBlockList();//all blocks
 	PathSolver* solver = new PathSolver(origin, destination,blocks);//from origin to destination
---
+++ run.bat
@@ -0,0 +1,1 @@
+C:\Users\zhoulei\Documents\GitHub\UKMOcompetition\x64\Debug\UKMO_UAVRouting.exe 1 2
